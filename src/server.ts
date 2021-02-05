ab111fc237fc6df039e5d70201872cc351c21935
Anderson <chltjdrhd777@naver.com>
2021-02-05 10:03:07 +0900

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import cookieparser from "cookie-parser";
 //initializing app
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieparser());
 
 app.get("/", (req, res) => {
   res.json({ message: "hello this is a test page" });
