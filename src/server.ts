738a612624ac514e6278648fa00dc56023af33ae
Anderson <chltjdrhd777@naver.com>
2021-02-05 09:50:04 +0900

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
-
+import cors from "cors";
+//initializing app
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "hello this is a test page" });
