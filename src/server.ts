40ca6d92c2caaa2b1c47069f316f56a9a10e2d8a
Anderson <chltjdrhd777@naver.com>
2021-02-05 10:08:58 +0900

    Update

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,27 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import cookieparser from "cookie-parser";
+import mongoose from "mongoose";
+const mongoDBAccessKey = require("./config/key");
 //initializing app
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieparser());
+
+//initializing mongoose
+mongoose
+  .connect(mongoDBAccessKey.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("monogoDB is connected");
+  })
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.json({ message: "hello this is a test page" });
