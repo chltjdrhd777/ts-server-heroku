d7c4f0e27c5d63faeb9e3e6b87d6303580a93cfe
Anderson <chltjdrhd777@naver.com>
2021-02-05 11:13:05 +0900

    Update

---
+++ src/model/UserModel.ts
@@ -0,0 +1,92 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+//typeDef
+export interface UserBaseDocumentType extends Document {
+  name: string;
+  email: string;
+  password: string;
+  lastname: string;
+  role: number;
+  art: [];
+  history: [];
+  image: string;
+  token: string;
+  tokenExp: string;
+  comparePassword(plainPassword: string): Promise<boolean>;
+  generateToken(): Promise<UserBaseDocumentType>;
+}
+
+interface UserStatics extends Model<UserBaseDocumentType> {
+  findByToken(token: string, callback: any): void;
+}
+
+//Schema
+const userSchema: Schema<
+  UserBaseDocumentType,
+  UserStatics
+> = new mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minglength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  cart: {
+    type: Array,
+    default: [],
+  },
+  history: {
+    type: Array,
+    default: [],
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+//methods
+userSchema.pre<UserBaseDocumentType>("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+//export
+export default mongoose.model<UserBaseDocumentType, UserStatics>(
+  "User",
+  userSchema
+);
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,25 @@
-import express, { Router, Response } from "express";
+import express, { Router, Request, Response } from "express";
+import User, { UserBaseDocumentType } from "../model/UserModel";
 
 const router = Router();
 
-router.get("/register", (req, res) => {
-  res.json({ messgae: "work" });
+//typeDef
+interface CumstomRequest extends Request<{}, {}, UserBaseDocumentType> {}
+
+router.post("/register", (req: CumstomRequest, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.json({
+      success: false,
+      message: "u didn't put the email or password",
+    });
+  }
+
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false });
+
+    return res.status(200).json({ success: true, doc });
+  });
 });
 
 export default router;
